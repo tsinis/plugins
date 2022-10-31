898df2638416ce5eed71eb7b23587717f6c63cfc
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2022-10-31 16:39:08 +0100

    docs(ios): added documentation for custom ui image picker

--- packages/image_picker/image_picker_ios/ios/Classes/CUIImagePickerController.h
+++ packages/image_picker/image_picker_ios/ios/Classes/CUIImagePickerController.h
@@ -8,6 +8,8 @@
 // Basic import to inherit from [UIImagePickerController] class.
 #import <UIKit/UIKit.h>
 
+// Interface inherited from [UIImagePickerController], will be used
+// only for overriding default values of [UIImagePickerController].
 @interface CUIImagePickerController : UIImagePickerController
 
 @end
--- packages/image_picker/image_picker_ios/ios/Classes/CUIImagePickerController.m
+++ packages/image_picker/image_picker_ios/ios/Classes/CUIImagePickerController.m
@@ -5,20 +5,32 @@
 //  Copyright 2022 Roman Cinis. All rights reserved.
 //
 
-// Basic import to inherit from [UIImagePickerController] class.
 #import "CUIImagePickerController.h"
 
+// Implementation of the [CUIImagePickerController] class.
 @implementation CUIImagePickerController
+
+// [shouldAutorotate] is depricated on iOS 16+, but is used for in older systems.
 - (BOOL)shouldAutorotate {
   return NO;
 }
 
--(NSUInteger)supportedInterfaceOrientations {
-  return UIInterfaceOrientationMaskPortrait;
+// Same as [shouldAutorotate] it's depricated on iOS 16+, but is used for in older systems.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    return (interfaceOrientation == UIInterfaceOrientationLandscapeLeft);
 }
 
+
+// The interface orientations that the view controller supports.
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
+  return UIInterfaceOrientationMaskLandscape;
+}
+
+
+// The prefered orientation to use in camera view (default to landscape left)
+// View will be rotated to this one if it's not in landscape already.
 - (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation {
-  return UIInterfaceOrientationPortrait;
+  return UIInterfaceOrientationLandscapeLeft;
 }
 
 @end
